d8ce5cd23f56c61b409d20e296bb92a1f3a7b911
markiplagat <markkiplagatkipruttoh@gmail.com>
2021-02-11 02:31:12 +0300

    Password reset

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,10 @@ Rails.application.routes.draw do
   post "sign_in", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
 
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
+
   root to: "main#index"
 end
